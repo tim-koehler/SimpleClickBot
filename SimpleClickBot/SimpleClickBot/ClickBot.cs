7f5e692787d48fb21ba75a0181b7fc85905436ee
Tim Koehler <tim@gitlabexample.com>
2020-03-28 13:40:45 +0100

    Added missing files

---
+++ SimpleClickBot/SimpleClickBot/ClickBot.cs
@@ -0,0 +1,45 @@
+﻿using SimpleClickBot.Properties;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SimpleClickBot
+{
+    public class ClickBot
+    {
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void Setup()
+        {
+            Thread clickThread = new Thread(DoClick);
+            clickThread.IsBackground = true;
+            clickThread.Start();
+        }
+
+        private static void DoClick()
+        {
+            while (true)
+            {
+                if (KeyListener.IsKeyPressed)
+                {
+                    PerformClick();
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private static void PerformClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            Thread.Sleep(5);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Thread.Sleep(Settings.Default.SettingDelay);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+    }
+}
---
+++ SimpleClickBot/SimpleClickBot/KeyListener.cs
@@ -0,0 +1,37 @@
+﻿using SimpleClickBot.Properties;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SimpleClickBot
+{
+    public static class KeyListener
+    {
+        public static bool IsKeyPressed { get; private set; } = false;
+
+        public static void Setup()
+        {
+            Thread listenThread = new Thread(DoListenForKeys);
+            listenThread.IsBackground = true;
+            listenThread.Start();
+        }
+
+        private static void DoListenForKeys()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Settings.Default.SettingTriggerKey) != 0)
+                {
+                    IsKeyPressed = true;
+                }
+                else
+                {
+                    IsKeyPressed = false;
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+    }
+}
